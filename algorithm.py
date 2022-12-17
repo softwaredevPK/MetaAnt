cbfcf0a8b0187e3a79bc2045f2db88b73a7d0b52
softwaredevPK <71095058+softwaredevPK@users.noreply.github.com>
2022-12-17 21:44:30 +0100

    Algorythm finished

--- algorithm.py
+++ algorithm.py
@@ -2,14 +2,18 @@ import pandas as pd
 import numpy as np
 from Haversine import haversine
 import random
+import warnings
+from draw_image import GraphDrawer
+warnings.filterwarnings("ignore")
 
 
 class Vehicle():
 
-    def __init__(self, weight_limit, start_city_index):
-        self.route = [start_city_index]
+    def __init__(self, weight_limit):
+        self.route = []
         self.weight_limit = weight_limit
         self.current_weight = 0
+        self.distance = 0
 
     # done
     def increase_weight(self, weight):
@@ -24,28 +28,36 @@ class Vehicle():
             from_city = to_city
         return distance
 
+    # done
+    def get_remaining_weight_limit(self):
+        return self.weight_limit - self.current_weight
+
 
 class Car(Vehicle):
-    ...
+    pass
 
 
 class Ant(Vehicle):
-    ...
+    pass
 
 
 class AntAlgorithm:
 
     def __init__(self, pheromone_weight=1, visibility_weight=2, start_city_index=0, evaporation_coefficient=0.5):
+        if not 0 <= evaporation_coefficient <= 1:
+            raise KeyError("evaporation_coefficient should be from [0,1]")
         self.distance_matrix = None
+        self.demand_array = None
         self.alfa = pheromone_weight
         self.beta = visibility_weight
         self.start_city_index = start_city_index
         self.evaporation = evaporation_coefficient
+        self.cities_data = None
 
     # done
     def read_csv(self, filepath):
         """Expects a csv file with 4 columns: city, lon, lat, demand without headers"""
-        return pd.read_csv(filepath, names=["lon", "lat", "demand"])
+        return pd.read_csv(filepath, names=["city", "lon", "lat", "demand"])
 
     # done
     def calculate_distance_matrix(self, cities_data):
@@ -58,11 +70,12 @@ class AntAlgorithm:
             distance_matrix.append(city_array)
         return pd.DataFrame(distance_matrix)
 
-    def get_demand_matrix(self, cities_data):
+    # done
+    def get_demand_array(self, cities_data):
         demand = []
         for city, data in cities_data.iterrows():
             demand.append(data["demand"])
-        return demand
+        return pd.array(demand)
 
     # done
     def get_initial_visibility(self):
@@ -90,12 +103,12 @@ class AntAlgorithm:
     def calculate_possibility_of_visit(self, pheromone_matrix, probability_matrix):
         matrix_size = len(self.distance_matrix)
         visit_matrix = pheromone_matrix ** self.alfa * probability_matrix ** self.beta
-        possibilities = visit_matrix / (pd.DataFrame(np.ones([matrix_size, matrix_size])) * visit_matrix.sum(1)).transpose()
+        possibilities = (visit_matrix / (pd.DataFrame(np.ones([matrix_size, matrix_size])) * visit_matrix.sum(1)).transpose()).replace(np.NaN, 0)
         return possibilities.cumsum(1)  # cumulative
 
     # done
     def update_pheromone_matrix(self, ants, pheromones_matrix):
-        evaporation_matrix = pheromones_matrix - self.evaporation
+        evaporation_matrix = (1 - self.evaporation) * pheromones_matrix
         for ant in ants:
             from_city = ant.route[0]
             for to_city in ant.route[1:]:
@@ -103,38 +116,63 @@ class AntAlgorithm:
                 from_city = to_city
         return evaporation_matrix
 
+    # done
+    def update_visibility_by_demand(self, visibility_matrix, weight_limit):
+        cities_to_skip = visibility_matrix[self.demand_array > weight_limit].index
+        for city in cities_to_skip:
+            visibility_matrix[city] = 0
+        return visibility_matrix
+
+    # done
+    def get_best_ant_route(self, ants):
+        best_ant = None
+        best_distance = None
+        for ant in ants:
+            distance = ant.get_route_distance(self.distance_matrix)
+            if best_ant is None or distance < best_distance:
+                best_distance = distance
+                best_ant = ant
+        return best_ant, best_distance
+
+    # done
+    def update_initial_visibility_matrix_by_used_routes(self, visibility_matrix, routes):
+        for route in routes:
+            for visited_city in route[1:]:
+                visibility_matrix[visited_city] = 0
+        return visibility_matrix
+
+    # done
     def calculate(self, cars, cycles_no, ants_no, csv_file):
-        cities_data = self.read_csv(csv_file)
-        self.distance_matrix = self.calculate_distance_matrix(cities_data)
-
-        # todo tymczasowe na czas testów
-        self.distance_matrix = pd.DataFrame(
-            [[0, 10, 12, 11, 14], [10, 0, 13, 15, 8], [12, 13, 0, 9, 14], [11, 15, 9, 0, 16], [14, 8, 14, 16, 0]])
-
-        # todo brakuje handlingu zapotrzebowania i pojemności - przydałby się słownik {"miasto": "zapotrzebowanie"}
+        self.cities_data = self.read_csv(csv_file)
+        self.distance_matrix = self.calculate_distance_matrix(self.cities_data)
+        self.demand_array = self.get_demand_array(self.cities_data)
         used_routes = []
-        for car_number, car in enumerate(cars):
-            best_route = None
 
-            demand_matrix = self.get_demand_matrix(cities_data)
+        for car_number, car in enumerate(cars):
+            best_distance = None
+            best_ant = None
+            if car_number == 0:
+                initial_visibility_matrix = self.get_initial_visibility()
 
             for cycle in range(cycles_no):
-                best_cycle_route = None
-                ants = [Ant(car.weight_limit, self.start_city_index) for i in range(ants_no)]
+                ants = [Ant(car.weight_limit) for i in range(ants_no)]
 
                 # preparation
                 if cycle == 0:
                     pheromones_matrix = self.get_initial_pheromones()
                 for ant in ants:
+
+                    ant.route.append(self.start_city_index)
                     # Step 1
-                    visibility_matrix = self.get_initial_visibility()
-                    if car_number != 0:
-                        ...
-                        # todo chyba tutaj trzeba updejtować raz jeszcze visibility o 0 dla miast które były już odwiedzone, dzięki temu ich nie wybierze(przez used_routes)
+                    visibility_matrix = initial_visibility_matrix.copy()
+
                     while True:
-                        if len(ant.route) == len(self.distance_matrix):
-                            break
                         visibility_matrix = self.update_visibility(visibility_matrix, ant)
+                        visibility_matrix = self.update_visibility_by_demand(visibility_matrix, ant.get_remaining_weight_limit())
+
+                        if (visibility_matrix == 0).all().all():
+                            # there is nowhere to go
+                            break
 
                         # Step 2
                         cumulitive_possibility_matrix = self.calculate_possibility_of_visit(pheromones_matrix, visibility_matrix)
@@ -144,23 +182,91 @@ class AntAlgorithm:
                         cumulitive_values_from_current_city = cumulitive_possibility_matrix.iloc[ant.route[-1]]
                         new_city_to_go = cumulitive_values_from_current_city[cumulitive_values_from_current_city > random_value].index[0]
 
-                        if (demand_matrix[new_city_to_go] > car.weight_limit):
-                            # todo: powinniśmy znaleźć inne miasto jeśli to możliwe
-                            continue
-                        car.weight_limit -= demand_matrix[new_city_to_go]
-                        demand_matrix[new_city_to_go] = 0
-
                         ant.route.append(new_city_to_go)
+                        ant.increase_weight(self.demand_array[new_city_to_go])
+
+                    # Each ant is supposed to return to start point
+                    ant.route.append(self.start_city_index)
+
                 # step 4
                 # update pheromone level for next cycle
                 pheromones_matrix = self.update_pheromone_matrix(ants, pheromones_matrix)
 
-            # end of cycles
-            car.route = best_cycle_route  # todo jeszcze nie wiem jak się wybiera ostateczną i najlepsza trase, na koniec cykli? w trakcie cykli? do rozkminy
+                # best route from cycle
+                best_cycle_ant, best_cycle_distance = self.get_best_ant_route(ants)
+                if best_ant is None or best_cycle_distance < best_distance:
+                    best_distance = best_cycle_distance
+                    best_ant = best_cycle_ant
+
+            # end of all cycles
+
+            # save best route from experiment to car
+            car.route = best_ant.route
+            car.current_weight = best_ant.current_weight
+            car.distance = best_distance
+            used_routes.append(best_ant.route)
+
+            # next car is not supposed to go to the same cities
+            initial_visibility_matrix = self.update_initial_visibility_matrix_by_used_routes(initial_visibility_matrix,
+                                                                                             used_routes)
+
+        return cars, sum([i.distance for i in cars])
+
+    def draw_image(self, paths):
+        cities_data = self.cities_data
+        GraphDrawer(paths, cities_data=cities_data).run()
+
+
+"""
+W celu lepszego pokazania rysowania, zedytowoaliśmy TestData by się współrzędne nie nakłądały na siebie
+Czyli odległości - punkty na mapie są fałszywe dla tego zbioru!
+"""
 
 
 
-# todo aktualnie nie mamy powrotu do bazy(miasta 0), jeśli mmay dodać to wystarczy na końcu poza while do routa poprzez append startowa jak sie nie myle
+print('TestData:')
+to_draw_algorithm = AntAlgorithm()
+to_draw_solved_cars, distance = to_draw_algorithm.calculate([Vehicle(1000) for i in range(0, 3)], 5, 8, "TestData.csv")
+print("Sum of distance:", distance)
+for car in to_draw_solved_cars:
+    print(car.route, car.current_weight, car.distance)
 
+
+print('TestData: start city 2')
+# works with different city to start, with more cars than required
+algorithm = AntAlgorithm(start_city_index=2)
+solved_cars, distance = algorithm.calculate([Vehicle(1000) for i in range(0, 5)], 5, 8, "TestData.csv")
+print("Sum of distance:", distance)
+for car in solved_cars:
+    print(car.route, car.current_weight, car.distance)
+
+print('TownsData, 5 cycles, 8 ants')
+# works with different city to start, with more cars than required
 algorithm = AntAlgorithm()
-algorithm.calculate([Vehicle(1000, 0) for i in range(0, 5)], 5, 5, "TestData.csv")
+solved_cars, distance = algorithm.calculate([Vehicle(1000) for i in range(0, 5)], 5, 8, "TownsData.csv")
+print("Sum of distance:", distance)
+for car in solved_cars:
+    print(car.route, car.current_weight, car.distance)
+
+
+print('TownsData, 5cycles,  30 ants')
+# works with different city to start, with more cars than required
+algorithm = AntAlgorithm()
+solved_cars, distance = algorithm.calculate([Vehicle(1000) for i in range(0, 5)], 5, 8, "TownsData.csv")
+print("Sum of distance:", distance)
+for car in solved_cars:
+    print(car.route, car.current_weight, car.distance)
+
+
+print('TownsData, 10cycles, 20 ants')
+# works with different city to start, with more cars than required
+algorithm = AntAlgorithm()
+solved_cars, distance = algorithm.calculate([Vehicle(1000) for i in range(0, 5)], 10, 20, "TownsData.csv")
+print("Sum of distance:", distance)
+for car in solved_cars:
+    print(car.route, car.current_weight, car.distance)
+
+
+
+# drawing first test data
+to_draw_algorithm.draw_image([i.route for i in to_draw_solved_cars])
--- draw_image.py
+++ draw_image.py
@@ -1,65 +1,46 @@
 import matplotlib.pyplot as plt
-from multiprocessing import Queue as MPQueue
 
-# todo do przejżenia i poprawienia
 
-class VrptwAcoFigure:
-    def __init__(self, nodes: list, path_queue: MPQueue):
-        self.nodes = nodes
+class GraphDrawer:
+    def __init__(self, paths: list, cities_data):
+        self.paths = paths
         self.figure = plt.figure(figsize=(10, 10))
         self.figure_ax = self.figure.add_subplot(1, 1, 1)
-        self.path_queue = path_queue
+        self.cities_data = cities_data
         self._depot_color = 'k'
         self._customer_color = 'steelblue'
-        self._line_color = 'darksalmon'
+        self.colors = ["red", "orange", "blue", "dark", "purple"]
 
-    def _draw_point(self):
-        # 画出depot
-        self.figure_ax.scatter([self.nodes[0].x], [self.nodes[0].y], c=self._depot_color, label='depot', s=40)
+    def _draw_points(self):
+        x = round(self.cities_data.iloc[0]['lon'], 0)
+        y = round(self.cities_data.iloc[0]['lat'], 0)
+        self.figure_ax.scatter([x], [y], c=self._depot_color, label='depot', s=40)
 
-        # 画出customer
-        self.figure_ax.scatter(list(node.x for node in self.nodes[1:]),
-                               list(node.y for node in self.nodes[1:]), c=self._customer_color, label='customer', s=20)
+        remaining_x = [round(self.cities_data.iloc[i]['lon'], 0) for i in self.cities_data.index[1:]]
+        remaining_y = [round(self.cities_data.iloc[i]['lat'], 0) for i in self.cities_data.index[1:]]
+        self.figure_ax.scatter(remaining_x,
+                               remaining_y, c=self._customer_color, label='customer', s=20)
         plt.pause(0.5)
 
     def run(self):
-        # 先绘制出各个结点
-        self._draw_point()
+        self._draw_points()
         self.figure.show()
 
-        # 从队列中读取新的path，进行绘制
-        while True:
-            if not self.path_queue.empty():
-                # 取队列中最新的一个path，其他的path丢弃
-                info = self.path_queue.get()
-                while not self.path_queue.empty():
-                    info = self.path_queue.get()
+        for i, path in enumerate(self.paths):
+            try:
+                color = self.colors[i]
+            except KeyError:
+                color = self.colors[-1]
+            self._draw_line(path, color)
+        plt.pause(100)
 
-                path, distance, used_vehicle_num = info.get_path_info()
-                if path is None:
-                    print('[draw figure]: exit')
-                    break
-
-                # 需要先记录要移除的line，不能直接在第一个循环中进行remove，
-                # 不然self.figure_ax.lines会在循环的过程中改变，导致部分line无法成功remove
-                remove_obj = []
-                for line in self.figure_ax.lines:
-                    if line._label == 'line':
-                        remove_obj.append(line)
-
-                for line in remove_obj:
-                    self.figure_ax.lines.remove(line)
-                remove_obj.clear()
-
-                # 重新绘制line
-                self.figure_ax.set_title('travel distance: %0.2f, number of vehicles: %d ' % (distance, used_vehicle_num))
-                self._draw_line(path)
-            plt.pause(1)
-
-    def _draw_line(self, path):
-        # 根据path中index进行路径的绘制
+    def _draw_line(self, path, color="darksalmon"):
         for i in range(1, len(path)):
-            x_list = [self.nodes[path[i - 1]].x, self.nodes[path[i]].x]
-            y_list = [self.nodes[path[i - 1]].y, self.nodes[path[i]].y]
-            self.figure_ax.plot(x_list, y_list, color=self._line_color, linewidth=1.5, label='line')
-            plt.pause(0.2)
+            x1 = round(self.cities_data.iloc[path[i - 1]]['lon'], 0)
+            y1 = round(self.cities_data.iloc[path[i - 1]]['lat'], 0)
+            x2 = round(self.cities_data.iloc[path[i]]['lon'], 0)
+            y2 = round(self.cities_data.iloc[path[i]]['lat'], 0)
+            x_list = [x1, x2]
+            y_list = [y1, y2]
+            self.figure_ax.plot(x_list, y_list, color=color, linewidth=1.5, label='line')
+            plt.pause(0.35)
